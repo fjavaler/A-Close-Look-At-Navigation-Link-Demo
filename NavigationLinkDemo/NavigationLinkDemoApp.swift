b470b9a9f9f79b61d3af80b1cfc40123d7bbc95c
Frederick Javalera <frederickjavalera@gmail.com>
2021-04-05 21:41:52 -0600

    Init commit

---
+++ NavigationLinkDemo/NavigationLinkDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationLinkDemoApp.swift
+//  NavigationLinkDemo
+//
+//  Created by Frederick Javalera on 4/5/21.
+//
+
+import SwiftUI
+
+@main
+struct NavigationLinkDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
